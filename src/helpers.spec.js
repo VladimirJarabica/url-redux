c36eece7651a39ea9490d86afbee5374face899a
VladimirJarabica <vladojarabica@gmail.com>
2018-01-18 12:24:20 +0100

    initial version

---
+++ src/helpers.spec.js
@@ -0,0 +1,40 @@
+import R from 'ramda';
+
+import { parseQuery, makeQuery } from './main';
+
+describe('#urlHelper', () => {
+  describe('#makeQuery', () => {
+    it('should make simple query', () => {
+      expect(makeQuery({ kek: 'bur' })).toEqual('?kek=bur');
+    });
+    it('should make query with multiple params', () => {
+      expect(makeQuery({ kek: 'bur', lol: 1234 })).toEqual('?kek=bur&lol=1234');
+    });
+  });
+  describe('#parseQuery', () => {
+    it('should parse simple query', () => {
+      expect(parseQuery('kek=bur')).toEqual({ kek: 'bur' });
+      expect(parseQuery('?kek=bur')).toEqual({ kek: 'bur' });
+    });
+    it('should parse query with multiple params', () => {
+      expect(parseQuery('kek=bur&lol=1234')).toEqual({
+        kek: 'bur',
+        lol: '1234',
+      });
+      expect(parseQuery('?kek=bur&lol=1234')).toEqual({
+        kek: 'bur',
+        lol: '1234',
+      });
+    });
+  });
+  describe('#deterministic', () => {
+    it('make => parse', () => {
+      const input = { kek: 'bur', lol: '1234' };
+      expect(R.compose(parseQuery, makeQuery)(input)).toEqual(input);
+    });
+    it('parse => make', () => {
+      const input = '?kek=bur&lol=1234';
+      expect(R.compose(makeQuery, parseQuery)(input)).toEqual(input);
+    });
+  });
+});
---
+++ src/main.js
@@ -0,0 +1,88 @@
+// @flow
+import R from 'ramda';
+
+// =======
+// Actions
+type AddParam = {|
+  type: 'url-redux/ADD_PARAM',
+  payload: {
+    key: string,
+    param: string,
+  },
+|};
+type RemoveParam = {|
+  type: 'url-redux/REMOVE_PARAM',
+  payload: {
+    key: string,
+  },
+|};
+
+type Action = AddParam | RemoveParam;
+
+type Url = { [key: string]: string }; // TODO: add support for arrays
+
+export const addParam = (key: string, param: string): AddParam => ({
+  type: 'url-redux/ADD_PARAM',
+  payload: { key, param },
+});
+
+export const removeParam = (key: string): RemoveParam => ({
+  type: 'url-redux/REMOVE_PARAM',
+  payload: { key },
+});
+
+// ================
+// Helper functions
+export const makeQuery = (params: Url): string =>
+  R.compose(
+    R.concat('?'),
+    R.join('&'),
+    R.map(
+      key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`,
+    ),
+    R.keys,
+  )(params);
+
+export const parseQuery = (query: string): Url =>
+  R.compose(
+    R.reduce((acc, keyParam) => {
+      const [key, param] = R.split('=')(keyParam);
+      return R.assoc(key, param, acc);
+    }, {}),
+    R.split('&'),
+    R.ifElse(query => R.head(query) === '?', R.tail, R.identity),
+  )(query);
+
+// ======
+// Reducer
+const INITIAL_STATE = R.compose(
+  parseQuery,
+  R.pathOr('', ['location', 'search']),
+)(window);
+
+const urlReducer = (state: Url = INITIAL_STATE, action: Action): Url => {
+  if (!window) return state; // Server rendering
+  switch (action.type) {
+    case 'url-redux/ADD_PARAM':
+      const addedParams = R.assoc(
+        action.payload.key,
+        action.payload.param,
+        state.url,
+      );
+      window.location.search = makeQuery(addedParams);
+      return addedParams;
+    case 'url-redux/REMOVE_PARAM':
+      const removedParams = R.dissoc(action.payload.key, state);
+      window.location.search = makeQuery(removedParams);
+      return removedParams;
+    default:
+      return state;
+  }
+};
+
+// ========
+// Selector
+// Can be used only if this reducer is under "url"
+export const getUrlParams = (state: { url: Url }): Url => state.url;
+
+export default urlReducer;
